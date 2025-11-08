23faeca233f824263c5652c7b7775fd424de3242
alexmaryin <java73@yandex.ru>
2025-11-08 11:18:41 +0300

    Desktop language change fixed with Key function wrapping root UI and observing language property.

--- composeApp/src/commonMain/kotlin/io/github/alexmaryin/followmymus/rootNavigation/ui/RootContent.kt
+++ composeApp/src/commonMain/kotlin/io/github/alexmaryin/followmymus/rootNavigation/ui/RootContent.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
@@ -11,6 +15,7 @@ import com.arkivanov.decompose.extensions.compose.stack.animation.plus
 import com.arkivanov.decompose.extensions.compose.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
 import io.github.alexmaryin.followmymus.core.changeLanguage
+import io.github.alexmaryin.followmymus.core.ui.isDesktop
 import io.github.alexmaryin.followmymus.core.ui.theme.FollowMyMusTheme
 import io.github.alexmaryin.followmymus.preferences.Language
 import io.github.alexmaryin.followmymus.preferences.ThemeMode
@@ -31,6 +36,7 @@ fun RootContent(component: RootComponent) {
     val preferences = rememberAppPreferences(datastore)
     val theme by preferences.getThemeMode().collectAsStateWithLifecycle(ThemeMode.SYSTEM)
     val isDark = theme == ThemeMode.DARK || (theme == ThemeMode.SYSTEM && isSystemInDarkTheme())
+    var appLanguage by remember { mutableStateOf(Language.SYSTEM) }
 
     LaunchedEffect(Unit) {
         preferences.getLanguage().collectLatest { language ->
@@ -40,20 +46,27 @@ fun RootContent(component: RootComponent) {
                 Language.SYSTEM -> null
             }
             changeLanguage(lang)
+            appLanguage = language
         }
     }
 
     FollowMyMusTheme(darkTheme = isDark) {
-        Children(
-            stack = component.childStack,
-            animation = stackAnimation(slide() + fade())
-        ) {
-            when (val child = it.instance) {
-                is Child.Splash -> SplashScreen()
-                is Child.LoginChild -> LoginScreen(child.component)
-                is Child.MainScreenPager -> MainScreen(child.component)
-                is Child.SignUpChild -> SignUpScreen(child.component)
+        AddOnlyDesktopLanguageKey(appLanguage) {
+            Children(
+                stack = component.childStack,
+                animation = stackAnimation(slide() + fade())
+            ) {
+                when (val child = it.instance) {
+                    is Child.Splash -> SplashScreen()
+                    is Child.LoginChild -> LoginScreen(child.component)
+                    is Child.MainScreenPager -> MainScreen(child.component)
+                    is Child.SignUpChild -> SignUpScreen(child.component)
+                }
             }
         }
     }
 }
+
+@Composable
+inline fun AddOnlyDesktopLanguageKey(key: Any?, content: @Composable () -> Unit) =
+    if (isDesktop()) key(key) { content() } else content()
